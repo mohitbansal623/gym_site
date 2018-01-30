a1542252ef5b548d842e3b478d6e90d32a50a7a9
ankitatayal <ankita.tayal@innoraft.com>
2018-01-30 22:50:30 +0530

    coach dashboard

--- sites/all/modules/custom/client_log_notify_coach/templates/coach_dashboard.tpl.php
+++ sites/all/modules/custom/client_log_notify_coach/templates/coach_dashboard.tpl.php
@@ -8,29 +8,31 @@
 
 <?php else :?>
   <h1 class="page-header">Client Information</h1>
-  <div id="accordion" class="panel-group coach-accordion">
-  <?php foreach ($client_info as $key => $value): ?>
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h4 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#<?php echo $key; ?>"><?php echo $value['full_name']; ?></a>
-        </h4>
-      </div>
-      <div id="<?php echo $key; ?>" class="panel-collapse collapse">
-        <div class="panel-body">
-          <p><strong>Goal:</strong> <?php echo $value['goal']; ?></p>
-          <p><strong>Remaining Days:</strong> <?php echo $value['remaining_days']; ?></p>
-          <p><strong>Body Fat Loss:</strong> <?php echo $value['bodyfat_lost']; ?></p>
-          <p><strong>Diet Adherence:</strong> <?php echo $value['diet_adherence']; ?></p>
-          <p><strong>CM Loss:</strong> <?php echo $value['cm_loss']; ?></p>
-          <p><strong>MM Loss:</strong> <?php echo $value['mm_loss']; ?></p>
-          <p><strong>Average Daily Activity:</strong> <?php echo $value['avg_daily_activity']; ?></p>
-          <p><strong>Average Training Sessions:</strong> <?php echo $value['avg_training_sessions']; ?></p>
-          <p><a href="<?php echo $value['link']; ?>">Click to see profile.</a></p>
+  <div class="row">
+    <div id="accordion" class="panel-group coach-accordion">
+      <?php foreach ($client_info as $key => $value): ?>
+        <div class="panel panel-default col-sm-4 col-xs-12">
+          <div class="panel-heading">
+            <h4 class="panel-title">
+              <a data-toggle="collapse" data-parent="#accordion" href="#<?php echo $key; ?>"><?php echo $value['full_name']; ?></a>
+            </h4>
+          </div>
+          <div id="<?php echo $key; ?>" class="panel-collapse collapse">
+            <div class="panel-body">
+              <p><strong>Goal:</strong> <?php echo $value['goal']; ?></p>
+              <p><strong>Remaining Days:</strong> <?php echo $value['remaining_days']; ?></p>
+              <p><strong>Body Fat Loss:</strong> <?php echo $value['bodyfat_lost']; ?></p>
+              <p><strong>Diet Adherence:</strong> <?php echo $value['diet_adherence']; ?></p>
+              <p><strong>CM Loss:</strong> <?php echo $value['cm_loss']; ?></p>
+              <p><strong>MM Loss:</strong> <?php echo $value['mm_loss']; ?></p>
+              <p><strong>Average Daily Activity:</strong> <?php echo $value['avg_daily_activity']; ?></p>
+              <p><strong>Average Training Sessions:</strong> <?php echo $value['avg_training_sessions']; ?></p>
+              <p><a href="<?php echo $value['link']; ?>">Click to see profile.</a></p>
+            </div>
+          </div>
         </div>
-      </div>
+      <?php endforeach; ?>
     </div>
-  <?php endforeach; ?>
-</div>
+  </div>
 
 <?php endif; ?>
